35bc9177e953fedebf1c14e027b3660c4cc0094f
Jay179-sudo <jayprasad2002@gmail.com>
2024-06-13 15:56:54 +0530

    (MINOR) feat: added K8s resource call capability

--- cmd/api/vm-request.go
+++ cmd/api/vm-request.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"Jay179-sudo/backend-api-infra/internal/data"
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -23,7 +25,7 @@ func (app *application) requestVM(w http.ResponseWriter, r *http.Request) {
 		app.errorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	// // validate email using regex
+	// validate email using regex
 
 	// Object will enclose and its name/label will be the email provided - (RAM|STORAGE|SERVICE)
 	// 1. RAM ------> 	Request + Limit
@@ -33,12 +35,13 @@ func (app *application) requestVM(w http.ResponseWriter, r *http.Request) {
 	// send request to the kubernetes API
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalf("Could not authenticate with the API. Error: %v", err.Error())
+		log.Printf("Could not authenticate with the API. Error: %v", err.Error())
 		http.Error(w, "first The server encountered an error while processing your data", http.StatusInternalServerError)
+		return
 	}
-	_, err = kubernetes.NewForConfig(config)
+	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Could not instantiate clientset. Error: %v", err.Error())
+		log.Printf("Could not instantiate clientset. Error: %v", err.Error())
 		http.Error(w, "second-lastThe server encountered an error while processing your data", http.StatusInternalServerError)
 		return
 	}
@@ -51,18 +54,50 @@ func (app *application) requestVM(w http.ResponseWriter, r *http.Request) {
 		}
 		reducedEmail += string(element)
 	}
-	reducedEmail += "pod"
 	secondsLeft := time.Until(VMRequest.Spec.ExpiryTime).Seconds()
 	if secondsLeft > float64(MaxAllotedTime) || secondsLeft < 0 {
 		secondsLeft = float64(MaxAllotedTime)
 	}
 	allotedSeconds := strconv.Itoa(int(secondsLeft))
 	log.Println(allotedSeconds)
-
+	userRequestSpec := data.UserRequestSpec{
+		Email:     VMRequest.Email,
+		RAM:       VMRequest.Spec.RAM,
+		CPU:       2,
+		PublicKey: VMRequest.Spec.PublicKey,
+	}
+	userRequest := data.UserRequest{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "UserRequest",
+			APIVersion: "request.jaypd.github.com/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      reducedEmail + "-userrequest",
+			Namespace: "default",
+		},
+		Spec: userRequestSpec,
+	}
+	userReqBody, err := json.Marshal(userRequest)
+	if err != nil {
+		app.WriteJson(w, http.StatusInternalServerError, envelope{"status": "Internal Server Error"}, nil)
+		log.Printf("Could not create the resource %v:", err.Error())
+		return
+	}
+	// Create UserRequest Resource
+	response := client.RESTClient().
+		Post().
+		AbsPath("apis/request.jaypd.github.com/v1/namespaces/default/userrequests").
+		Body(userReqBody).
+		Do(context.TODO())
+	if err = response.Error(); err != nil {
+		app.WriteJson(w, http.StatusInternalServerError, envelope{"status": "Internal Server Error"}, nil)
+		log.Printf("Could not create the resource %v:", err.Error())
+		return
+	}
 	err = app.WriteJson(w, http.StatusOK, envelope{"status": "Request queued!"}, nil)
 	if err != nil {
 		app.WriteJson(w, http.StatusInternalServerError, envelope{"status": "Internal Server Error"}, nil)
-		log.Fatalf("Could not create the Pod %v", err.Error())
+		log.Printf("Could not create the resource %v:", err.Error())
 		return
 	}
 }
--- internal/data/data-types.go
+++ internal/data/data-types.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type Specification struct {
 	RAM        int32     `json:"RAM"`
@@ -11,4 +15,28 @@ type Specification struct {
 type VMRequest struct {
 	Email string        `json:"Email"`
 	Spec  Specification `json:"Specification"`
+}
+
+// UserRequestSpec defines the desired state of UserRequest
+type UserRequestSpec struct {
+	Email     string `json:"Email"`
+	RAM       int32  `json:"RAM"`
+	CPU       int32  `json:"CPU"`
+	PublicKey string `json:"PublicKey"`
+	// volume added later on
+}
+
+// UserRequestStatus defines the observed state of UserRequest
+type UserRequestStatus struct {
+	// reference to all services
+	ActivePort int32 `json:"ActivePort"`
+}
+
+// UserRequest is the Schema for the userrequests API
+type UserRequest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   UserRequestSpec   `json:"spec,omitempty"`
+	Status UserRequestStatus `json:"status,omitempty"`
 }
